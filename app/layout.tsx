abfe622059832e76b8d658357edc56538ad8e642
matthew.raga <matthew.raga@tuitt.com>
2025-07-24 23:12:47 +0800

    replace favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Matthew Ramon Raga',
-  description: 'Full Stack Developer',
+  title: "Matthew Ramon Raga",
+  description: "Full Stack Developer",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
